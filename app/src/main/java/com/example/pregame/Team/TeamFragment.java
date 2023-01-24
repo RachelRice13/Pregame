d497a0383fe5a0b1aea910f5e594eadad5d9f05c
RachelRice13 <ricerachel12@gmail.com>
2023-01-24 13:22:51 +0000

    Added coaches join teams functionality

--- app/src/main/java/com/example/pregame/Team/TeamFragment.java
+++ app/src/main/java/com/example/pregame/Team/TeamFragment.java
@@ -19,8 +19,10 @@ import android.widget.Toast;
 import com.example.pregame.Model.Coach;
 import com.example.pregame.Model.Team;
 import com.example.pregame.R;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -28,12 +30,16 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.UUID;
 
 public class TeamFragment extends Fragment {
-    public static final String TAG = "CreateTeam";
+    public static final String TAG_CREATE = "CreateTeam";
+    public static final String TAG_TEAM = "TeamInfo";
+    public static final String TAG_JOIN = "JoinTeam";
     private View view;
     private FirebaseFirestore firebaseFirestore;
     private FirebaseAuth firebaseAuth;
@@ -55,19 +61,19 @@ public class TeamFragment extends Fragment {
 
         buildRecyclerView();
 
-        FloatingActionButton createTeam = view.findViewById(R.id.create_team_button);
-        createTeam.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton createTeamButton = view.findViewById(R.id.create_team_button);
+        createTeamButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 createTeam();
             }
         });
 
-        FloatingActionButton joinTeam = view.findViewById(R.id.join_team_button);
-        joinTeam.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton joinTeamButton = view.findViewById(R.id.join_team_button);
+        joinTeamButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                joinTeam();
             }
         });
 
@@ -98,7 +104,7 @@ public class TeamFragment extends Fragment {
                                     .addOnFailureListener(new OnFailureListener() {
                                         @Override
                                         public void onFailure(@NonNull Exception e) {
-                                            Log.e(TAG, e.toString());
+                                            Log.e(TAG_TEAM, e.toString());
                                         }
                                     });
                         }
@@ -146,7 +152,7 @@ public class TeamFragment extends Fragment {
                         .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                             @Override
                             public void onSuccess(DocumentReference documentReference) {
-                                Log.d(TAG, "Successfully added new team to database");
+                                Log.d(TAG_CREATE, "Successfully added new team to database");
                                 addTeamToCoach(documentReference.getId());
                                 Toast.makeText(getContext(), "Successfully created " + teamName, Toast.LENGTH_SHORT).show();
                                 alert.cancel();
@@ -158,7 +164,7 @@ public class TeamFragment extends Fragment {
                         .addOnFailureListener(new OnFailureListener() {
                             @Override
                             public void onFailure(@NonNull Exception e) {
-                                Log.e(TAG, "ERROR: " + e.getMessage());
+                                Log.e(TAG_CREATE, "ERROR: " + e.getMessage());
                             }
                         });
             }
@@ -178,13 +184,122 @@ public class TeamFragment extends Fragment {
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void unused) {
-                        Log.d(TAG, "Added team to coach array");
+                        Log.d(TAG_CREATE, "Added team to coach array");
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.e(TAG, e.toString());
+                        Log.e(TAG_CREATE, e.toString());
+                    }
+                });
+    }
+
+    public void joinTeam() {
+        LayoutInflater layoutInflater = LayoutInflater.from(getContext());
+        View joinTeamV = layoutInflater.inflate(R.layout.join_new_team, null);
+        AlertDialog.Builder joinTeamAD = new AlertDialog.Builder(getContext());
+
+        EditText teamCode1 = joinTeamV.findViewById(R.id.team_code_1);
+        EditText teamCode2 = joinTeamV.findViewById(R.id.team_code_2);
+        EditText teamCode3 = joinTeamV.findViewById(R.id.team_code_3);
+        EditText teamCode4 = joinTeamV.findViewById(R.id.team_code_4);
+        EditText teamCode5 = joinTeamV.findViewById(R.id.team_code_5);
+        EditText teamCode6 = joinTeamV.findViewById(R.id.team_code_6);
+        EditText teamCode7 = joinTeamV.findViewById(R.id.team_code_7);
+        EditText teamCode8 = joinTeamV.findViewById(R.id.team_code_8);
+
+        Button joinTeam = joinTeamV.findViewById(R.id.join_new_team_button);
+        FloatingActionButton goBack = joinTeamV.findViewById(R.id.join_team_go_back);
+
+        joinTeamAD.setCancelable(false).setView(joinTeamV);
+        AlertDialog alert = joinTeamAD.create();
+        alert.show();
+
+        joinTeam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String teamCode = teamCode1.getText().toString() + teamCode2.getText().toString() + teamCode3.getText().toString() + teamCode4.getText().toString() + teamCode5.getText().toString() + teamCode6.getText().toString() + teamCode7.getText().toString() + teamCode8.getText().toString();
+
+                // Search through collection
+                firebaseFirestore.collection("team").whereEqualTo("teamCode", teamCode)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Team team = document.toObject(Team.class);
+
+                                        // Display alert builder, asking "Is this the team you want to join?"
+
+                                        // Check and see if coach already exists
+                                        boolean containsCoach = containsCoach(team);
+
+                                        if (containsCoach) {
+                                            Toast.makeText(getContext(), currentUser.getEmail() + " is already a coach for '" + team.getTeamName() + "'", Toast.LENGTH_LONG).show();
+                                        } else {
+                                            // Add Team Reference to Coach Array of Teams
+                                            addTeamToCoach(document.getId());
+                                            teams.clear();
+                                            getTeamsInfo();
+
+                                            // Add Coach Reference to Team Array of Coaches
+                                            addCoachToTeam(document.getId());
+
+                                            Toast.makeText(getContext(), "Successfully added " + currentUser.getEmail() + " to " + team.getTeamName(), Toast.LENGTH_SHORT).show();
+                                            alert.cancel();
+                                        }
+
+                                        Log.d(TAG_JOIN, document.getId() + " => " + team.getTeamName());
+                                    }
+                                } else {
+                                    Log.e(TAG_JOIN, "Error getting documents: ", task.getException());
+                                    Toast.makeText(getContext(), "That team code doesn't exist", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alert.cancel();
+            }
+        });
+    }
+
+    public boolean containsCoach(Team team) {
+        boolean found = false;
+        for (int i = 0; i < team.getCoaches().size(); i++) {
+            DocumentReference reference = team.getCoaches().get(i);
+            if (reference.getId().equals(currentUser.getUid())) {
+                Log.e(TAG_JOIN, "Team contains coach " + currentUser.getEmail());
+                found = true;
+                break;
+            } else {
+                Log.e(TAG_JOIN, "Team doesn't contains coach " + currentUser.getEmail());
+                found =  false;
+            }
+        }
+        return found;
+    }
+
+    public void addCoachToTeam(String teamId) {
+        DocumentReference coach = firebaseFirestore.collection("coach").document(currentUser.getUid());
+
+        firebaseFirestore.collection("team").document(teamId).update("coaches", FieldValue.arrayUnion(coach))
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d(TAG_JOIN, "Added coach to team array");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG_JOIN, e.toString());
                     }
                 });
     }
